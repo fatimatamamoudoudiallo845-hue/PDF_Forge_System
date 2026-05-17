22cc015b42d3d7ef8f7ac3cba19cb984ed79d628
fatimatamamoudoudiallo845-hue <fatimatamamoudoudiallo845@gmail.com>
2026-05-17 14:31:48 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ echo "✓ Java OK"
 
 # ── Démarrer orbd ───────────────────────────────────────
 echo "[1] Démarrage orbd (port 1050)..."
-orbd -ORBInitialPort 1050 &
+/usr/lib/jvm/java-1.8.0-amazon-corretto/bin/orbd -ORBInitialPort 1050 &
 ORBD_PID=$!
 sleep 3
 
